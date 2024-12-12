89aec303cd4d3c37a429b1bcc6f11f40314b7db8
Yurii Tykhovskyi <yuriity@outlook.com>
2024-12-12 15:14:05 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fbm-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'fretboard-map';
+}
